783426abd198762b221baf674ee74b7832bde1ed
Shahiduzzamansakib <159687977+Shahiduzzamansakib@users.noreply.github.com>
2024-05-17 16:51:53 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+let bar = document.querySelector('.bar');
+let sidebar = document.querySelector('.sidebar');
+let cross = document.querySelector('.cross');
+
+
+
+bar.addEventListener('click',function(){
+
+  sidebar.classList.add('active');
+ 
+  
+});
+
+cross.addEventListener('click',function(){
+
+  sidebar.classList.remove('active');
+  
+});
+
+sidebar.addEventListener('click',function(e){
+
+  if(e.target.classList.contains('active') == true){
+
+    sidebar.classList.remove('active');
+  
+  };
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
